7ca1e62a61d0590e716b367d052e0a4a41c3153a
Bernhard Schuster <bernhard@ahoi.io>
2020-05-26 12:15:45 +0200

    feat/span: add Eq + Hash impl to Spans

--- src/span.rs
+++ src/span.rs
@@ -2,18 +2,40 @@
 
 pub use proc_macro2::LineColumn;
 
+use std::hash::{Hasher, Hash};
+
+
 /// Relative span in relation
 /// to the beginning of a doc comment.
-#[derive(Clone, Debug, Copy)]
+#[derive(Clone, Debug, Copy, PartialEq, Eq)]
 // TODO ,Eq,PartialEq,PartialOrd,Ord
 pub struct RelativeSpan {
     pub start: LineColumn,
     pub end: LineColumn,
 }
 
+impl Hash for RelativeSpan {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.start.line.hash(state);
+        self.start.column.hash(state);
+        self.end.line.hash(state);
+        self.end.column.hash(state);
+    }
+}
+
+
 // Span in relation to a full Document
-#[derive(Clone, Debug, Copy)]
+#[derive(Clone, Debug, Copy, PartialEq, Eq)]
 pub struct Span {
     pub start: LineColumn,
     pub end: LineColumn,
+}
+
+impl Hash for Span {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.start.line.hash(state);
+        self.start.column.hash(state);
+        self.end.line.hash(state);
+        self.end.column.hash(state);
+    }
 }
