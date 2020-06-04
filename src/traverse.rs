d6c3f45fd26f5048b5614a3bf5fc0f671e8e9353
Bernhard Schuster <bernhard@ahoi.io>
2020-06-04 17:18:11 +0200

    feat/interactive: partial impl of interactively applying fixes
    Does not write the changes back to files just yet.

--- src/traverse.rs
+++ src/traverse.rs
@@ -332,7 +332,122 @@ pub(crate) fn run(
                 }
             }
         }
-        Mode::Interactive => unimplemented!("Interactive pick & apply is not implemented just yet"),
+        Mode::Interactive => {
+            // show a horizontal list of replacements, navigate left/ right by using the arrow keys
+            // .. suggestion0 [suggestion1] suggestion2 suggestion3 ..
+            // arrow left
+            // .. suggestion1 [suggestion2] suggestion3 suggestion4 ..
+            use crossterm;
+
+            use crossterm::{
+                cursor, event::Event, event::KeyCode, event::KeyEvent, style::Print,
+                QueueableCommand, Result,
+            };
+            use std::io::{stdout, Write};
+
+            const HELP: &'static str = r##"y - apply this suggestion
+n - do not apply the suggested correction
+q - quit; do not stage this hunk or any of the remaining ones
+a - stage this hunk and all later hunks in the file
+d - do not apply this suggestion and skip the rest of the file
+g - select a suggestion to go to
+j - leave this hunk undecided, see next undecided hunk
+J - leave this hunk undecided, see next hunk
+e - manually edit the current hunk
+? - print help
+"##;
+
+            // @todo cluster by file
+            let mut stdout = stdout();
+
+            let mut apply = indexmap::IndexSet::<Suggestion<'_>>::new();
+
+            for (path, suggestions) in suggestions_per_path {
+                let count = suggestions.len();
+                println!("Path is {} and has {}", path.display(), count);
+
+                // juck, uggly
+                let mut suggestions_it = suggestions.clone().into_iter().enumerate();
+
+                #[derive(Debug, Clone, Copy)]
+                enum Direction {
+                    Forward,
+                    Backward,
+                }
+                let mut direction = Direction::Forward;
+                loop {
+                    let opt: Option<(usize, Suggestion)> = match direction {
+                        Forward => suggestions_it.next(),
+                        Backward => suggestions_it.next_back(),
+                    };
+
+                    trace!("next() ---> {:?}", &opt);
+
+
+                    if opt.is_none() {
+                        match direction {
+                            Direction::Forward => {
+                                trace!("completed file, continue to next");
+                                break // we completed this file, move on to the next
+                            }
+                            Direction::Backward => {
+                                trace!("went back, now back at the beginning");
+                                suggestions_it = suggestions.clone().into_iter().enumerate();
+                                continue;
+                            } // go to the start
+                        }
+                    }
+                    let (idx, suggestion) = opt.expect("Must be X");
+
+                    println!("{}", suggestion);
+
+                    println!(
+                        "({nth}/{of_n}) Apply this suggestion [y,n,q,a,d,j,e,?]?",
+                        nth = idx,
+                        of_n = count
+                    );
+
+                    // read is blocking
+                    let event = if let Event::Key(event) =
+                        crossterm::event::read().map_err(|e| {
+                            anyhow::anyhow!("Something unexpected happened on the CLI: {}", e)
+                        })? {
+                        event
+                    } else {
+                        trace!("read() something othe than a key event an error");
+                        break;
+                    };
+                    let KeyEvent { code, modifiers } = event;
+
+                    match code {
+                        KeyCode::Char('y') => {
+                            apply.insert(suggestion);
+                        }
+                        KeyCode::Char('n') => {}
+                        KeyCode::Char('q') => return Ok(()),
+                        KeyCode::Char('a') => {
+                            for (_, suggestion) in suggestions_it {
+                                apply.insert(suggestion);
+                            }
+                            break;
+                        }
+                        KeyCode::Char('d') => break,
+                        KeyCode::Char('j') => {
+                            direction = Direction::Backward;
+                            continue;
+                        }
+                        KeyCode::Char('e') => unimplemented!("Manual editing"),
+                        KeyCode::Char('?') => {
+                            println!("{}", HELP);
+                        }
+                        x => {
+                            trace!("Unexpected input {:?}", x);
+                        }
+                    }
+                    direction = Direction::Forward;
+                }
+            }
+        }
     }
 
     Ok(())
