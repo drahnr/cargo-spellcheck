51c34f03b1191135093e3def0c9ff00143c450a3
Bernhard Schuster <bernhard@ahoi.io>
2020-06-22 17:58:31 +0200

    refactor/documentation: split chunk and cluster from documentation
    Separation of concerns allows for better testing, this is a WIP commit.

---
+++ src/documentation/cluster.rs
@@ -0,0 +1,100 @@
+//! Cluster `proc_macro2::Literal`s into `LiteralSets`
+
+use super::*;
+use indexmap::IndexMap;
+use anyhow::{anyhow, Result, Error};
+use crate::{Range, Span};
+use super::chunk::{ContentSource, CheckableChunk};
+
+/// Cluster literals for one file
+#[derive(Debug)]
+pub struct Clusters {
+    pub(super) set: Vec<LiteralSet>,
+}
+
+impl Clusters {
+    /// Only works if the file is processed line by line, otherwise
+    /// requires a adjacency list.
+    fn process_literal(&mut self, literal: proc_macro2::Literal) {
+        let literal = TrimmedLiteral::from(literal);
+        if let Some(ref mut cls) = self.set.last_mut() {
+            if let Err(literal) = cls.add_adjacent(literal) {
+                trace!(target: "documentation",
+                    "appending, but failed to append: {:?} to set {:?}",
+                    &literal,
+                    &cls
+                );
+                cls.push(LiteralSet::from(literal))
+            } else {
+                trace!("successfully appended to existing: {:?} to set", &cls);
+            }
+        } else {
+            // the very first literal
+            self.set = vec![LiteralSet::from(literal)];
+        }
+    }
+
+    /// Helper function to parse a stream and associated the found literals
+    fn parse_token_tree(&mut self, stream: proc_macro2::TokenStream) {
+        let mut iter = stream.into_iter();
+        while let Some(tree) = iter.next() {
+            match tree {
+                TokenTree::Ident(ident) => {
+                    // if we find an identifier
+                    // which is doc
+                    if ident != "doc" {
+                        continue;
+                    }
+
+                    // this assures the sequence is as anticipated
+                    let op = iter.next();
+                    if op.is_none() {
+                        continue;
+                    }
+                    let op = op.unwrap();
+                    if let TokenTree::Punct(punct) = op {
+                        if punct.as_char() != '=' {
+                            continue;
+                        }
+                        if punct.spacing() != Spacing::Alone {
+                            continue;
+                        }
+                    } else {
+                        continue;
+                    }
+
+                    let comment = iter.next();
+                    if comment.is_none() {
+                        continue;
+                    }
+                    let comment = comment.unwrap();
+                    if let TokenTree::Literal(literal) = comment {
+                        trace!(target: "documentation",
+                            "Found doc literal at {:?}: {:?}",
+                            <Span as TryInto<Range>>::try_into(Span::from(literal.span())),
+                            literal
+                        );
+                        self.process_literal(literal);
+                    } else {
+                        continue;
+                    }
+                }
+                TokenTree::Group(group) => {
+                    self.parse_token_tree(group.stream());
+                }
+                _ => {}
+            };
+        }
+    }
+}
+
+impl From<proc_macro2::TokenStream> for Clusters
+{
+    fn from( stream: proc_macro2::TokenStream) -> Self {
+        let mut chunk = Self {
+            set: Vec::with_capacity(64),
+        };
+        chunk.parse_token_tree(stream);
+        chunk
+    }
+}
--- src/documentation/mod.rs
+++ src/documentation/mod.rs
@@ -14,12 +14,17 @@ use proc_macro2::{Spacing, TokenTree};
 pub use proc_macro2::LineColumn;
 use std::path::{Path, PathBuf};
 
+mod chunk;
+mod cluster;
+
+pub use chunk::*;
+pub use cluster::*;
+
 /// Collection of all the documentation entries across the project
 #[derive(Debug, Clone)]
 pub struct Documentation {
     /// Mapping of a path to documentation literals
-    // @todo add an intermediate enum to be able to handle markdown files as part of a document too
-    index: IndexMap<PathBuf, Vec<LiteralSet>>,
+    index: IndexMap<ContentSource, Vec<CheckableChunk>>,
 }
 
 impl Documentation {
@@ -33,136 +38,57 @@ impl Documentation {
         self.index.is_empty()
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = (&PathBuf, &Vec<LiteralSet>)> {
+    pub fn iter(&self) -> impl Iterator<Item = (&ContentSource, &Vec<CheckableChunk>)> {
         self.index.iter()
     }
 
-    pub fn into_iter(self) -> impl Iterator<Item = (PathBuf, Vec<LiteralSet>)> {
+    pub fn into_iter(self) -> impl Iterator<Item = (ContentSource, Vec<CheckableChunk>)> {
         self.index.into_iter()
     }
 
     pub fn join(&mut self, other: Documentation) -> &mut Self {
         other
             .into_iter()
-            .for_each(|(path, mut literals): (_, Vec<LiteralSet>)| {
-                self.index
-                    .entry(path)
-                    .and_modify(|acc: &mut Vec<LiteralSet>| {
-                        acc.append(&mut literals);
-                    })
-                    .or_insert_with(|| literals);
+            .for_each(|(source, chunks): (_, Vec<CheckableChunk>)| {
+                let _ = self.add(source, chunks);
             });
         self
     }
 
-    pub fn combine(mut docs: Vec<Documentation>) -> Documentation {
-        if let Some(first) = docs.pop() {
-            docs.into_iter().fold(first, |mut first, other| {
-                first.join(other);
-                first
+    pub fn extend<I,J,T>(&mut self, mut docs: I)
+    where
+        I: IntoIterator<Item = Documentation, IntoIter=J>,
+        J: Iterator<Item = Documentation>,
+        T: Eq,
+    {
+        docs.into_iter().for_each(|other| {
+            self.join(other);
+        });
+    }
+
+    pub fn add(&mut self, source: ContentSource, chunks: Vec<CheckableChunk>) {
+        self.index
+            .entry(source)
+            .and_modify(|acc: &mut Vec<CheckableChunk>| {
+                acc.append(&mut chunks);
             })
-        } else {
-            Documentation::new()
-        }
-    }
-
-    /// Append a literal to the given path
-    ///
-    /// Only works if the file is processed line by line, otherwise
-    /// requires a adjacency list.
-    pub fn append_literal(&mut self, path: &Path, literal: proc_macro2::Literal) {
-        let literal = TrimmedLiteral::from(literal);
-        match self.index.entry(path.to_owned()) {
-            indexmap::map::Entry::Occupied(occupied) => {
-                let v = occupied.into_mut();
-                let cls = v.last_mut().unwrap();
-                if let Err(literal) = cls.add_adjacent(literal) {
-                    trace!(target: "documentation",
-                        "appending, but failed to append: {:?} to set {:?}",
-                        &literal,
-                        &cls
-                    );
-                    v.push(LiteralSet::from(literal))
-                } else {
-                    trace!("successfully appended to existing: {:?} to set", &cls);
-                }
-            }
-            indexmap::map::Entry::Vacant(vacant) => {
-                trace!(target: "documentation",
-                    "nothing for {} file yet, create new literal set",
-                    path.display()
-                );
-                vacant.insert(vec![LiteralSet::from(literal)]);
-            }
-        }
-    }
-
-    /// Helper function to parse a path stream and associated the found literals to `path`
-    fn parse_token_tree<P: AsRef<Path>>(&mut self, path: P, stream: proc_macro2::TokenStream) {
-        let path: &Path = path.as_ref();
-
-        let mut iter = stream.into_iter();
-        while let Some(tree) = iter.next() {
-            match tree {
-                TokenTree::Ident(ident) => {
-                    // if we find an identifier
-                    // which is doc
-                    if ident != "doc" {
-                        continue;
-                    }
-
-                    // this assures the sequence is as anticipated
-                    let op = iter.next();
-                    if op.is_none() {
-                        continue;
-                    }
-                    let op = op.unwrap();
-                    if let TokenTree::Punct(punct) = op {
-                        if punct.as_char() != '=' {
-                            continue;
-                        }
-                        if punct.spacing() != Spacing::Alone {
-                            continue;
-                        }
-                    } else {
-                        continue;
-                    }
-
-                    let comment = iter.next();
-                    if comment.is_none() {
-                        continue;
-                    }
-                    let comment = comment.unwrap();
-                    if let TokenTree::Literal(literal) = comment {
-                        trace!(target: "documentation",
-                            "Found doc literal at {:?}: {:?}",
-                            <Span as TryInto<Range>>::try_into(Span::from(literal.span())),
-                            literal
-                        );
-                        self.append_literal(path, literal);
-                    } else {
-                        continue;
-                    }
-                }
-                TokenTree::Group(group) => {
-                    self.parse_token_tree(path, group.stream());
-                }
-                _ => {}
-            };
-        }
+            .or_insert_with(|| chunks);
+        // Ok(()) @todo make this failable
     }
 }
 
-impl<P> From<(P, proc_macro2::TokenStream)> for Documentation
-where
-    P: AsRef<Path>,
-{
-    fn from((path, stream): (P, proc_macro2::TokenStream)) -> Self {
-        let mut documentation = Documentation::new();
-        documentation.parse_token_tree(path, stream);
-        documentation
+
+/// only a shortcut to avoid duplicate code
+impl From<(ContentSource, proc_macro2::TokenStream)> for Documentation {
+    fn from((source, stream): (ContentSource, proc_macro2::TokenStream)) -> Self {
+        let cluster = Clusters::from(stream);
+        let chunks = Vec::<CheckableChunk>::from(cluster);
+        let mut docs = Documentation::new();
+        docs.add(source, chunks);
+        docs
     }
 }
+
 
 #[cfg(test)]
 mod tests {
