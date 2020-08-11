c63b0a0dd01f753f71eada9ef8c62c05506adba7
Bernhard Schuster <bernhard@ahoi.io>
2020-08-11 16:29:26 +0200

    feat/hunspell: implement transformation
    And as part of transformation, enable the whitelist feature.

--- src/checker/hunspell.rs
+++ src/checker/hunspell.rs
@@ -6,8 +6,13 @@
 //! Can handle multiple dictionaries.
 
 use super::{tokenize, Checker, Detector, Documentation, Suggestion, SuggestionSet};
+use crate::documentation::CheckableChunk;
+use crate::documentation::ContentOrigin;
+use crate::documentation::PlainOverlay;
 use crate::util::sub_chars;
-use log::{debug, trace};
+use crate::Range;
+use log::{debug, trace, warn};
+use std::collections::VecDeque;
 use std::path::PathBuf;
 
 use hunspell_rs::Hunspell;
@@ -16,18 +21,8 @@ use anyhow::{anyhow, bail, Result};
 
 pub struct HunspellChecker;
 
-impl Checker for HunspellChecker {
-    type Config = crate::config::HunspellConfig;
-    fn check<'a, 's>(docu: &'a Documentation, config: &Self::Config) -> Result<SuggestionSet<'s>>
-    where
-        'a: 's,
-    {
-        // let hunspell = lazy_static::lazy_static!{
-        //     static ref HUNSPELL_CTX: Result<Hunspell> = {
-
-        //     }
-        // };
-
+impl HunspellChecker {
+    fn inner_init(config: &<Self as Checker>::Config) -> Result<Hunspell> {
         let search_dirs = config.search_dirs();
 
         let lang = config.lang();
@@ -85,9 +80,9 @@ impl Checker for HunspellChecker {
 
         if cfg!(debug_assertions) && lang == "en_US" {
             // "Test" is a valid word
-            assert!(hunspell.check("Test"));
+            debug_assert!(hunspell.check("Test"));
             // suggestion must contain the word itself if it is valid
-            assert!(hunspell.suggest("Test").contains(&"Test".to_string()));
+            debug_assert!(hunspell.suggest("Test").contains(&"Test".to_string()));
         }
 
         // suggestion must contain the word itself if it is valid extra dictionary
@@ -111,48 +106,72 @@ impl Checker for HunspellChecker {
                 )
             }
         }
+        Ok(hunspell)
+    }
+}
+
+impl Checker for HunspellChecker {
+    type Config = crate::config::HunspellConfig;
+    fn check<'a, 's>(docu: &'a Documentation, config: &Self::Config) -> Result<SuggestionSet<'s>>
+    where
+        'a: 's,
+    {
+        let hunspell = Self::inner_init(config)?;
+
+        let transform_regex = config.transform_regex.clone().unwrap_or_else(|| Vec::new());
 
         let suggestions = docu.iter().try_fold::<SuggestionSet, _, Result<_>>(
             SuggestionSet::new(),
-            |mut acc, (origin, chunks)| {
+            move |mut acc, (origin, chunks)| {
                 debug!("Processing {}", origin.as_path().display());
+
+
+
                 for chunk in chunks {
                     let plain = chunk.erase_markdown();
                     trace!("{:?}", &plain);
                     let txt = plain.as_str();
                     for range in tokenize(txt) {
                         let word = sub_chars(txt, range.clone());
-                        if !hunspell.check(&word) {
-                            trace!("No match for word (plain range: {:?}): >{}<", &range, &word);
-                            // get rid of single character suggestions
-                            let replacements = hunspell
-                                .suggest(&word)
-                                .into_iter()
-                                .filter(|x| x.len() > 1) // single char suggestions tend to be useless
-                                .collect::<Vec<_>>();
-
-                            for (range, span) in plain.find_spans(range.clone()) {
-                                acc.add(
-                                    origin.clone(),
-                                    Suggestion {
-                                        detector: Detector::Hunspell,
-                                        range,
-                                        span,
-                                        origin: origin.clone(),
-                                        replacements: replacements.clone(),
-                                        chunk,
-                                        description: Some(
-                                            "Possible spelling mistake found.".to_owned(),
-                                        ),
-                                    },
-                                )
-                            }
+                        if transform_regex.is_empty() {
+                            obtain_suggestions(&plain, chunk, &hunspell, origin, word, range, &mut acc)
                         } else {
-                            trace!(
-                                "Found a match for word (plain range: {:?}): >{}<",
-                                &range,
-                                word
-                            );
+                            let mut matched = false;
+                            for regex in transform_regex.iter() {
+                                match regex.captures(word.as_str()) {
+                                    Ok(Some(captures)) => {
+                                        matched = true;
+                                        // first one is always the full match
+                                        if captures.len() == 1 {
+                                            // means match, but no captures,
+                                            // which is equiv to an implicit whitelist
+                                            break;
+                                        }
+                                        for m in captures.iter().skip(1).filter_map(|x| x) {
+                                            trace!("found capture for word >{}<, with match >{}< and capture >{}<", captures.get(0).unwrap().as_str(), word.as_str(), m.as_str());
+                                            let offset = word.char_indices().take_while(|(offset, _)| m.start() < *offset).count();
+                                            let range = Range {
+                                                start: range.start + offset,
+                                                end: range.start + offset + m.as_str().chars().count(),
+                                            };
+                                            obtain_suggestions(&plain, chunk, &hunspell, origin, m.as_str().to_owned(), range, &mut acc);
+                                        }
+                                        break;
+                                    },
+                                    Ok(None) => {
+                                        // no regex match, try the next regex
+                                        continue;
+                                    },
+                                    Err(e) => {
+                                        warn!("Matching regex >{}< errored: {}", regex.as_str(), e);
+                                        break;
+                                    }
+                                }
+                            }
+                            // nothing matched, check the entire word
+                            if !matched {
+                                obtain_suggestions(&plain, chunk, &hunspell, origin, word, range, &mut acc)
+                            }
                         }
                     }
                 }
@@ -162,5 +181,46 @@ impl Checker for HunspellChecker {
 
         // TODO sort spans by file and line + column
         Ok(suggestions)
+    }
+}
+
+fn obtain_suggestions<'s>(
+    plain: &PlainOverlay,
+    chunk: &'s CheckableChunk,
+    hunspell: &Hunspell,
+    origin: &ContentOrigin,
+    word: String,
+    range: Range,
+    acc: &mut SuggestionSet<'s>,
+) {
+    if !hunspell.check(&word) {
+        trace!("No match for word (plain range: {:?}): >{}<", &range, &word);
+        // get rid of single character suggestions
+        let replacements = hunspell
+            .suggest(&word)
+            .into_iter()
+            .filter(|x| x.len() > 1) // single char suggestions tend to be useless
+            .collect::<Vec<_>>();
+
+        for (range, span) in plain.find_spans(range.clone()) {
+            acc.add(
+                origin.clone(),
+                Suggestion {
+                    detector: Detector::Hunspell,
+                    range,
+                    span,
+                    origin: origin.clone(),
+                    replacements: replacements.clone(),
+                    chunk,
+                    description: Some("Possible spelling mistake found.".to_owned()),
+                },
+            )
+        }
+    } else {
+        trace!(
+            "Found a match for word (plain range: {:?}): >{}<",
+            &range,
+            word
+        );
     }
 }
