2c2be61334f0bc6c5b5ecfd7f272cea836c9588a
Bernhard Schuster <bernhard@ahoi.io>
2020-08-26 11:45:02 +0200

    refactor/reflow: split iterator into two

--- src/reflow/iter.rs
+++ src/reflow/iter.rs
@@ -1,8 +1,9 @@
 use super::*;
 
+use std::borrow::Cow;
 use std::collections::VecDeque;
 
-pub struct Warp<'s> {
+pub struct Tokeneer<'s> {
     /// Original source string of continuous lines which are to be wrapped.
     s: &'s str,
 
@@ -18,21 +19,16 @@ pub struct Warp<'s> {
     /// Ranges must be in characters!
     unbreakable_ranges: Vec<Range>,
     unbreakable_idx: usize,
-    max_line_width: usize,
 
-    // a temporary stack
-    queue: VecDeque<(Range, std::borrow::Cow<'s, str>)>,
     inner: std::iter::Peekable<std::iter::Enumerate<std::str::CharIndices<'s>>>,
     // in bytes
     previous_byte_offset: usize,
     // in chars!
     previous_char_offset: usize,
-    // line index we return
-    line_counter: usize,
 }
 
-impl<'s> Warp<'s> {
-    pub fn new(s: &'s str, range: Range, max_line_width: usize) -> Self {
+impl<'s> Tokeneer<'s> {
+    pub fn new(s: &'s str, range: Range) -> Self {
         let inner = s.char_indices().enumerate().peekable();
         Self {
             s,
@@ -40,49 +36,35 @@ impl<'s> Warp<'s> {
             spans: Vec::new(),
             unbreakable_ranges: Vec::new(),
             unbreakable_idx: 0usize,
-            queue: VecDeque::new(),
-            max_line_width,
             inner,
             previous_byte_offset: 0usize,
             previous_char_offset: 0usize,
-            line_counter: 0usize,
         }
     }
 
     pub fn add_unbreakables(&mut self, unbreakable_ranges: impl IntoIterator<Item = Range>) {
         self.unbreakable_ranges.extend(unbreakable_ranges);
     }
-
-    fn craft_line(&mut self) -> (usize, String, Range) {
-        use itertools::Itertools;
-        self.line_counter += 1;
-        let mut char_range = usize::MAX..0;
-        let line_content = self.queue.drain(..).map(|(range, s)| {
-            char_range.start = std::cmp::min(range.start, char_range.start);
-            char_range.end = std::cmp::max(range.end, char_range.end);
-            s
-        }).join(" ");
-        (self.line_counter, line_content, char_range)
-    }
 }
 
-impl<'s> Iterator for Warp<'s> {
+impl<'s> Iterator for Tokeneer<'s> {
     // Yields a tuple:
     //
     // * lineno
     // * content of that line
     // * and ranges relative to the original string which are covered by content
-    type Item = (usize, String, Range);
+    type Item = (Range, Range, Cow<'s, str>);
     fn next(&mut self) -> Option<Self::Item> {
-        let offset = 0usize; // @todo derive the indent based on the line spans
-        'outer: while let Some((char_idx,(byte_offset, c))) = self.inner.next() {
-            'unbreakable: while let Some(unbreakable) = self.unbreakable_ranges.get(self.unbreakable_idx) {
+        'outer: while let Some((char_idx, (byte_offset, c))) = self.inner.next() {
+            'unbreakable: while let Some(unbreakable) =
+                self.unbreakable_ranges.get(self.unbreakable_idx)
+            {
                 // are we within an unbreakable area
                 // iff so, just continue until we are out of the weeds
                 if unbreakable.contains(&char_idx) {
                     continue 'outer;
                 }
-                let idx= self.unbreakable_idx + 1;
+                let idx = self.unbreakable_idx + 1;
                 if let Some(unbreakable_next) = self.unbreakable_ranges.get(idx) {
                     // check if the next unbreakable is overlapping, should not, but hey
                     if unbreakable_next.contains(&char_idx) {
@@ -96,61 +78,130 @@ impl<'s> Iterator for Warp<'s> {
                 // we are out of the weeds, the next round needs the next index
                 self.unbreakable_idx = idx;
             }
-            
+
             if c.is_whitespace() {
                 // track the last whitespace, that plus one marks the start of the next word
                 self.previous_byte_offset = byte_offset + 1;
                 self.previous_char_offset = char_idx + 1;
                 continue 'outer;
             }
-            let byte_range = if let Some(&(char_idx, (byte_offset_next, c_next))) = self.inner.peek() {
-                if !c_next.is_whitespace() {
-                    // we have a non whitespace char ahead, so this is not a breaking position
-                    continue 'outer;
-                }
-                // if the next peek char is a whitespace, that means we reached the end of the word
-                self.previous_byte_offset..=byte_offset
-            } else if self.previous_byte_offset < byte_offset {
-                // peek is None, so we might be at the end of the string
-                // so we return the last part
-                self.previous_byte_offset..=byte_offset
-            } else {
-                // trailing whitespaces potentially
-                // @todo probably unreachable!("")
-                break 'outer;
-            };
+            let byte_range =
+                if let Some(&(char_idx, (byte_offset_next, c_next))) = self.inner.peek() {
+                    if !c_next.is_whitespace() {
+                        // we have a non whitespace char ahead, so this is not a breaking position
+                        continue 'outer;
+                    }
+                    // if the next peek char is a whitespace, that means we reached the end of the word
+                    self.previous_byte_offset..byte_offset_next
+                } else if self.previous_byte_offset < byte_offset {
+                    // peek is None, so we might be at the end of the string
+                    // so we return the last part
+                    self.previous_byte_offset..byte_offset
+                } else {
+                    // trailing whitespaces potentially
+                    // @todo probably unreachable!("")
+                    break 'outer;
+                };
 
-            let char_range = self.previous_char_offset..char_idx+1;
+            let char_range = self.previous_char_offset..char_idx + 1;
 
+            // the current item
+            // @todo refactor this into two iterators, one yielding the Cow<str> the other handling the reclustering
+            // into lines
+            let item = (
+                char_range.clone(),
+                byte_range.clone(),
+                std::borrow::Cow::Borrowed(dbg!(&self.s[byte_range])),
+            );
+            return Some(item);
+        }
+        None
+    }
+}
+
+/// Re-glue all tokenized items
+pub struct Gluon<'s> {
+    // char range + cow str
+    queue: VecDeque<(Range, std::borrow::Cow<'s, str>)>,
+    max_line_width: usize,
+    line_counter: usize,
+    inner: Tokeneer<'s>,
+}
+
+impl<'s> Gluon<'s> {
+    pub fn new(s: &'s str, range: Range, max_line_width: usize) -> Self {
+        Self {
+            queue: VecDeque::new(),
+            max_line_width,
+            line_counter: 0usize,
+            inner: Tokeneer::<'s>::new(s, range),
+        }
+    }
+
+    #[inline(always)]
+    pub fn add_unbreakables(&mut self, unbreakable_ranges: impl IntoIterator<Item = Range>) {
+        self.inner.add_unbreakables(unbreakable_ranges);
+    }
+
+    /// Create a new line based on the processing queue
+    fn craft_line(&mut self) -> (usize, String, Range) {
+        use itertools::Itertools;
+        self.line_counter += 1;
+        let mut char_range = usize::MAX..0;
+        let line_content = self
+            .queue
+            .drain(..)
+            .map(|(range, s)| {
+                char_range.start = std::cmp::min(range.start, char_range.start);
+                char_range.end = std::cmp::max(range.end, char_range.end);
+                s
+            })
+            .join(" ");
+        (self.line_counter, line_content, char_range)
+    }
+}
+
+impl<'s> Iterator for Gluon<'s> {
+    type Item = (usize, String, Range);
+    fn next(&mut self) -> Option<Self::Item> {
+        let offset = 0usize;
+        while let Some((char_range, byte_range, cow_str)) = self.inner.next() {
             // calculate the current characters that are already in that line
             // and assume one whitespace inbetween
             // @todo use a proper offset instead of the dummy
             let mut qiter = self.queue.iter();
             let acc_len = if let Some((first, _)) = qiter.next() {
-                qiter.map(|(r,_)| r.len()).fold(first.len(), |acc, len| { acc + 1 + len})
+                qiter
+                    .map(|(r, _)| r.len())
+                    .fold(first.len(), |acc, len| acc + 1 + len)
             } else {
                 0usize
             };
 
-            // the current item
-            // @todo refactor this into two iterators, one yielding the Cow<str> the other handling the reclustering
-            // into lines
-            let item = (char_range.clone(), std::borrow::Cow::Borrowed(dbg!(&self.s[byte_range])));
-
+            let item_len = char_range.len();
+            let item = (char_range.clone(), cow_str);
             let ret = if offset + acc_len <= self.max_line_width {
-                log::warn!("Line is longer than anticipated {} vs. {}", acc_len, self.max_line_width);
+                log::warn!(
+                    "Line is longer than anticipated {} vs. {}",
+                    acc_len,
+                    self.max_line_width
+                );
 
-                if offset + acc_len + char_range.len() >= self.max_line_width {
+                if offset + acc_len + item_len >= self.max_line_width {
                     // if the line length would be exceeded
                     let ret = self.craft_line();
                     self.queue.push_back(item);
                     ret
                 } else {
                     self.queue.push_back(item);
-                    continue 'outer;
+                    continue;
                 }
-            } else if char_range.len() > self.max_line_width {
-                log::warn!("A unbreakable chunk is larger than the max line width {} vs {}", char_range.len(), self.max_line_width);
+            } else if item_len > self.max_line_width {
+                log::warn!(
+                    "A unbreakable chunk is larger than the max line width {} vs {}",
+                    item_len,
+                    self.max_line_width
+                );
                 if acc_len > 0 {
                     // craft a line before inserting
                     // to avoid exceeding too much
@@ -160,7 +211,7 @@ impl<'s> Iterator for Warp<'s> {
                     line
                 } else {
                     self.queue.push_back(item);
-                    continue 'outer;
+                    continue;
                 }
             } else {
                 // the queue len is already larger than what the max line length allows
@@ -170,7 +221,6 @@ impl<'s> Iterator for Warp<'s> {
                 self.queue.push_back(item);
                 ret
             };
-
             return Some(ret);
         }
         None
@@ -191,11 +241,11 @@ mod tests {
             .lines()
             .enumerate()
             .map(|(idx, line)| (idx + 1, line));
-        let mut warp = Warp::new(content, 0..content.len(), max_line_width);
+        let mut gluon = Gluon::new(content, 0..content.len(), max_line_width);
 
-        warp.add_unbreakables(unbreakables);
+        gluon.add_unbreakables(unbreakables);
 
-        for ((line_no, line_content, _), (expected_no, expected_content)) in warp.zip(expected) {
+        for ((line_no, line_content, _), (expected_no, expected_content)) in gluon.zip(expected) {
             assert_eq!(line_no, expected_no);
             assert_eq!(line_content, expected_content);
         }
@@ -232,5 +282,13 @@ a single line"#;
         const EXPECTED: &'static str = r#"something kinda too
 Xong for a singlX line"#;
         verify_reflow(CONTENT, EXPECTED, 30usize, vec![20..37]);
+    }
+
+    #[test]
+    fn spaces_and_tabs() {
+        const CONTENT: &'static str = "        something     kinda       ";
+        const EXPECTED: &'static str = r#"something
+kinda"#;
+        verify_reflow(CONTENT, EXPECTED, 20usize, vec![]);
     }
 }
