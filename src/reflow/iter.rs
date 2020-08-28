9086d20a9817be600d4abb02dfd67f16a3d2db82
Bernhard Schuster <bernhard@ahoi.io>
2020-08-28 16:12:20 +0200

    feat/reflow: add indentation awareness to the iterators

--- src/reflow/iter.rs
+++ src/reflow/iter.rs
@@ -3,14 +3,15 @@ use super::*;
 use std::borrow::Cow;
 use std::collections::VecDeque;
 
+/// Tokenizes a section which is delimited by untokenizable content.
 pub struct Tokeneer<'s> {
     /// Original source string of continuous lines which are to be wrapped.
     s: &'s str,
 
     /// The sub-range of the string to reflow.
     range: Range,
-    /// the matching spans to that sub range
-    spans: Vec<Span>,
+    /// A set of matching spans to sub ranges.
+    spans: IndexMap<Range, Span>,
 
     /// If there would occur a line break, that falls within a range of this
     /// the break would only occur afterwards or the whole word gets moved to
@@ -28,12 +29,13 @@ pub struct Tokeneer<'s> {
 }
 
 impl<'s> Tokeneer<'s> {
+    /// Initialize a new tokenizer
     pub fn new(s: &'s str, range: Range) -> Self {
         let inner = s.char_indices().enumerate().peekable();
         Self {
             s,
             range,
-            spans: Vec::new(),
+            spans: Default::default(),
             unbreakable_ranges: Vec::new(),
             unbreakable_idx: 0usize,
             inner,
@@ -42,18 +44,16 @@ impl<'s> Tokeneer<'s> {
         }
     }
 
+    #[inline(always)]
     pub fn add_unbreakables(&mut self, unbreakable_ranges: impl IntoIterator<Item = Range>) {
         self.unbreakable_ranges.extend(unbreakable_ranges);
     }
 }
 
 impl<'s> Iterator for Tokeneer<'s> {
-    // Yields a tuple:
-    //
-    // * lineno
-    // * content of that line
-    // * and ranges relative to the original string which are covered by content
+    /// `(char range, byte range, )`
     type Item = (Range, Range, Cow<'s, str>);
+    /// Yields individual tokens with associated ranges in chars and bytes.
     fn next(&mut self) -> Option<Self::Item> {
         'outer: while let Some((char_idx, (byte_offset, c))) = self.inner.next() {
             'unbreakable: while let Some(unbreakable) =
@@ -85,33 +85,32 @@ impl<'s> Iterator for Tokeneer<'s> {
                 self.previous_char_offset = char_idx + 1;
                 continue 'outer;
             }
-            let byte_range =
-                if let Some(&(char_idx, (byte_offset_next, c_next))) = self.inner.peek() {
-                    if !c_next.is_whitespace() {
-                        // we have a non whitespace char ahead, so this is not a breaking position
-                        continue 'outer;
-                    }
-                    // if the next peek char is a whitespace, that means we reached the end of the word
-                    self.previous_byte_offset..byte_offset_next
-                } else if self.previous_byte_offset < byte_offset {
-                    // peek is None, so we might be at the end of the string
-                    // so we return the last part
-                    self.previous_byte_offset..byte_offset
-                } else {
-                    // trailing whitespaces potentially
-                    // @todo probably unreachable!("")
-                    break 'outer;
-                };
+            let byte_range = if let Some(&(_char_idx, (byte_offset_next, c_next))) =
+                self.inner.peek()
+            {
+                if !c_next.is_whitespace() {
+                    // we have a non whitespace char ahead, so this is not a breaking position
+                    continue 'outer;
+                }
+                // if the next peek char is a whitespace, that means we reached the end of the word
+                self.previous_byte_offset..byte_offset_next
+            } else if self.previous_byte_offset < byte_offset {
+                // peek is None, so we might be at the end of the string
+                // so we return the last part
+                self.previous_byte_offset..byte_offset
+            } else if cfg!(debug_assertions) {
+                unreachable!("Unreachable condition was reached");
+            } else {
+                log::error!("Should be never be reachable, please file a bug with the corresponding input data");
+                break 'outer;
+            };
 
             let char_range = self.previous_char_offset..char_idx + 1;
 
-            // the current item
-            // @todo refactor this into two iterators, one yielding the Cow<str> the other handling the reclustering
-            // into lines
             let item = (
                 char_range.clone(),
                 byte_range.clone(),
-                std::borrow::Cow::Borrowed(dbg!(&self.s[byte_range])),
+                std::borrow::Cow::Borrowed(&self.s[byte_range]),
             );
             return Some(item);
         }
@@ -119,26 +118,36 @@ impl<'s> Iterator for Tokeneer<'s> {
     }
 }
 
-/// Re-glue all tokenized items
+/// Re-glue all tokenized items under the constrained of a maximum line width
 pub struct Gluon<'s> {
-    // char range + cow str
+    /// Stores a sequence of undividable items as `(char range, cow str)`,
+    /// which are eventually combined to a new line.
     queue: VecDeque<(Range, std::borrow::Cow<'s, str>)>,
+    /// Maximum width in columns to be used before breaking a line.
     max_line_width: usize,
+    /// Internal counter for keeping track of the line index with base 1.
     line_counter: usize,
+    /// A set of indentations for considering indentations
+    /// if there are more lines than there are lines, the last
+    /// values in this vector willl bre reused.
+    indentations: Vec<usize>,
+    /// The inner iterator which first tokenizes the string into undividable items.
     inner: Tokeneer<'s>,
 }
 
 impl<'s> Gluon<'s> {
-    pub fn new(s: &'s str, range: Range, max_line_width: usize) -> Self {
+    pub fn new(s: &'s str, range: Range, max_line_width: usize, indentations: Vec<usize>) -> Self {
         Self {
             queue: VecDeque::new(),
             max_line_width,
+            indentations,
             line_counter: 0usize,
             inner: Tokeneer::<'s>::new(s, range),
         }
     }
 
     #[inline(always)]
+    #[allow(unused)]
     pub fn add_unbreakables(&mut self, unbreakable_ranges: impl IntoIterator<Item = Range>) {
         self.inner.add_unbreakables(unbreakable_ranges);
     }
@@ -162,13 +171,33 @@ impl<'s> Gluon<'s> {
 }
 
 impl<'s> Iterator for Gluon<'s> {
+    /// Returns a tuple of `(line number, line content, char range)`.
     type Item = (usize, String, Range);
+
     fn next(&mut self) -> Option<Self::Item> {
-        let offset = 0usize;
-        while let Some((char_range, byte_range, cow_str)) = self.inner.next() {
+        // obtain the indentation level for this line
+        // we do keep the indentation unless there are
+        // no sufficient lines which causes us to use
+        // the last used one
+        // Iff the whole thing was empty to begin with, `0`
+        // is the default fallback.
+        let offset = self
+            .indentations
+            .get(
+                // the next not yet crafted line
+                self.line_counter + 1,
+            )
+            .map(|x: &usize| *x)
+            .unwrap_or_else(|| {
+                self.indentations
+                    .last()
+                    .map(|x: &usize| *x)
+                    .unwrap_or(0usize)
+            });
+
+        while let Some((char_range, _byte_range, cow_str)) = self.inner.next() {
             // calculate the current characters that are already in that line
-            // and assume one whitespace inbetween
-            // @todo use a proper offset instead of the dummy
+            // and assume one whitespace inbetween each of them
             let mut qiter = self.queue.iter();
             let acc_len = if let Some((first, _)) = qiter.next() {
                 qiter
@@ -181,13 +210,9 @@ impl<'s> Iterator for Gluon<'s> {
             let item_len = char_range.len();
             let item = (char_range.clone(), cow_str);
             let ret = if offset + acc_len <= self.max_line_width {
-                log::warn!(
-                    "Line is longer than anticipated {} vs. {}",
-                    acc_len,
-                    self.max_line_width
-                );
-
-                if offset + acc_len + item_len >= self.max_line_width {
+                // calculate the sum if we would add the word
+                let sum = offset + acc_len + 1 + item_len;
+                if sum > self.max_line_width {
                     // if the line length would be exceeded
                     let ret = self.craft_line();
                     self.queue.push_back(item);
@@ -231,36 +256,38 @@ impl<'s> Iterator for Gluon<'s> {
 mod tests {
     use super::*;
 
+    /// `indentations` represent the indentation of indiviaul lines in `content`.
     fn verify_reflow(
         content: &'static str,
         expected: &'static str,
         max_line_width: usize,
         unbreakables: Vec<Range>,
+        indentations: Vec<usize>,
     ) {
         let expected = expected
             .lines()
             .enumerate()
             .map(|(idx, line)| (idx + 1, line));
-        let mut gluon = Gluon::new(content, 0..content.len(), max_line_width);
+        let mut gluon = Gluon::new(content, 0..content.len(), max_line_width, indentations);
 
         gluon.add_unbreakables(unbreakables);
 
         for ((line_no, line_content, _), (expected_no, expected_content)) in gluon.zip(expected) {
             assert_eq!(line_no, expected_no);
-            assert_eq!(line_content, expected_content);
+            assert_eq!(dbg!(line_content), dbg!(expected_content));
         }
     }
 
     #[test]
-    fn wrap_too_long() {
+    fn wrap_too_long_fluid() {
         const CONTENT: &'static str = "something kinda too long for a single line";
         const EXPECTED: &'static str = r#"something kinda too long for a
  single line"#;
-        verify_reflow(CONTENT, EXPECTED, 30usize, vec![]);
+        verify_reflow(CONTENT, EXPECTED, 30usize, vec![], vec![0]);
     }
 
     #[test]
-    fn wrap_too_short() {
+    fn wrap_too_short_fluid() {
         const CONTENT: &'static str = r#"something
 kinda
 too
@@ -273,7 +300,7 @@ line"#;
         const EXPECTED: &'static str = r#"something kinda too short for
 a single line"#;
 
-        verify_reflow(CONTENT, EXPECTED, 30usize, vec![]);
+        verify_reflow(CONTENT, EXPECTED, 30usize, vec![], vec![0; 8]);
     }
 
     #[test]
@@ -281,7 +308,7 @@ a single line"#;
         const CONTENT: &'static str = "something kinda too Xong for a singlX line";
         const EXPECTED: &'static str = r#"something kinda too
 Xong for a singlX line"#;
-        verify_reflow(CONTENT, EXPECTED, 30usize, vec![20..37]);
+        verify_reflow(CONTENT, EXPECTED, 30usize, vec![20..37], vec![0]);
     }
 
     #[test]
@@ -289,6 +316,29 @@ Xong for a singlX line"#;
         const CONTENT: &'static str = "        something     kinda       ";
         const EXPECTED: &'static str = r#"something
 kinda"#;
-        verify_reflow(CONTENT, EXPECTED, 20usize, vec![]);
+        verify_reflow(CONTENT, EXPECTED, 20usize, vec![], vec![0]);
+    }
+
+    #[test]
+    fn deep_indentation_too_long() {
+        const CONTENT: &'static str = r#"deep indentation"#;
+        const EXPECTED: &'static str = r#"deep
+indentation"#;
+        // 20 < 31 = 4 + 1 + 11 + 15
+        verify_reflow(CONTENT, EXPECTED, 20usize, vec![], vec![15]);
+    }
+
+    #[test]
+    fn deep_indentation_too_short() {
+        let _ = env_logger::builder()
+            .is_test(true)
+            .filter(None, log::LevelFilter::Trace)
+            .try_init();
+
+        const CONTENT: &'static str = r#"deep
+indentation"#;
+        const EXPECTED: &'static str = r#"deep indentation"#;
+        // 22 > 21 = 4 + 1 + 11 + 5
+        verify_reflow(CONTENT, EXPECTED, 22usize, vec![], vec![5, 5]);
     }
 }
--- src/reflow/mod.rs
+++ src/reflow/mod.rs
@@ -30,73 +30,7 @@ impl Checker for Reflow {
     where
         'a: 's,
     {
-        let wrapper = textwrap::Wrapper::new(config.max_line_length)
-            .subsequent_indent(" ")
-            .initial_indent(" ");
-        let suggestions = docu.iter().try_fold::<SuggestionSet, _, Result<_>>(
-            SuggestionSet::new(),
-            |mut acc, (origin, chunks)| {
-                for chunk in chunks {
-                    let mut new_lines = dbg!(&chunk)
-                        .as_str()
-                        .split("\n\n")
-                        .collect::<Vec<&str>>()
-                        .iter()
-                        .map(|s| s.replace("\n", ""))
-                        .fold::<Vec<String>, _>(Vec::new(), |mut acc, comment| {
-                            let mut new_comment = wrapper
-                                .wrap_iter(comment.trim())
-                                .map(|b| b.into_owned())
-                                .collect();
-                            acc.append(&mut new_comment);
-                            acc.push("".into());
-                            acc
-                        });
-                    // remove last newline
-                    let _ = new_lines.pop();
-                    // @todo that's too easy
-                    if new_lines.len() == chunk.as_str().lines().count() {
-                        log::trace!("No rewrapping required for '{:?}'", chunk);
-                        break; // the chunk did not change
-                    }
-                    // @todo find proper span and range
-                    let range = dbg!(0..chunk.as_str().len());
-                    let mut start = 1000..1001;
-                    let mut end = 0..1;
-                    let mut span = Span {
-                        start: LineColumn { line: 0, column: 0 },
-                        end: LineColumn { line: 0, column: 0 },
-                    };
-                    chunk.iter().for_each(|(r, s)| {
-                        if start.start > r.start {
-                            start = r.clone();
-                            span.start = s.start;
-                        }
-                        if end.end < r.end {
-                            end = r.clone();
-                            span.end = s.end;
-                        }
-                    });
-                    dbg!(&span);
-
-                    acc.add(
-                        origin.clone(),
-                        Suggestion {
-                            detector: Detector::Reflow,
-                            range,
-                            span,
-                            origin: origin.clone(),
-                            replacements: vec![new_lines.join("\n")],
-                            chunk,
-                            description: Some("Rewrapped".to_owned()),
-                        },
-                    )
-                }
-                Ok(acc)
-            },
-        )?;
-
-        Ok(suggestions)
+        unimplemented!("not yet")
     }
 }
 
