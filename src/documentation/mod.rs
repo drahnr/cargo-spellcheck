c31721666b4cbb157802bb493da6cd1401808b66
Bernhard Schuster <bernhard@ahoi.io>
2020-09-09 11:24:50 +0200

    refactor/documentation: move the handling of file types into documentation
    Replaces `add` with `add_rust` and `add_commonmark` with additional handling
    logic. `add` remains as `add_inner`, which is not publicly accessible.

--- src/documentation/mod.rs
+++ src/documentation/mod.rs
@@ -20,6 +20,7 @@ pub use proc_macro2::LineColumn;
 use proc_macro2::{Spacing, TokenTree};
 use std::convert::{TryFrom, TryInto};
 use std::path::PathBuf;
+use anyhow::{anyhow,Result};
 
 /// Range based on `usize`, simplification.
 pub type Range = core::ops::Range<usize>;
@@ -69,8 +70,8 @@ impl Documentation {
     pub fn join(&mut self, other: Documentation) -> &mut Self {
         other
             .into_iter()
-            .for_each(|(source, chunks): (_, Vec<CheckableChunk>)| {
-                let _ = self.add(source, chunks);
+            .for_each(|(origin, chunks): (_, Vec<CheckableChunk>)| {
+                let _ = self.add_inner(origin, chunks);
             });
         self
     }
@@ -87,14 +88,53 @@ impl Documentation {
     }
 
     /// Adds a set of `CheckableChunk`s to the documentation to be checked.
-    pub fn add(&mut self, source: ContentOrigin, mut chunks: Vec<CheckableChunk>) {
+    fn add_inner(&mut self, origin: ContentOrigin, mut chunks: Vec<CheckableChunk>) {
         self.index
-            .entry(source)
+            .entry(origin)
             .and_modify(|acc: &mut Vec<CheckableChunk>| {
                 acc.append(&mut chunks);
             })
             .or_insert_with(|| chunks);
         // Ok(()) TODO make this failable
+    }
+
+    /// Adds a rust content str to the documentation.
+    pub fn add_rust(&mut self, origin: ContentOrigin, content: &str) -> Result<()> {
+        let cluster = Clusters::try_from(content)?;
+
+        let chunks = Vec::<CheckableChunk>::from(cluster);
+        self.add_inner(origin, chunks);
+        Ok(())
+    }
+
+    /// Adds a common mark content str to the documentation.
+    pub fn add_commonmark(&mut self, origin: ContentOrigin, content: &str) -> Result<()> {
+        // extract the full content span and range
+        let start = LineColumn { line: 1, column: 0 };
+        let end = content
+            .lines()
+            .enumerate()
+            .last()
+            .map(|(idx, linecontent)| (idx + 1, linecontent))
+            .map(|(linenumber, linecontent)| LineColumn {
+                line: linenumber,
+                column: linecontent.chars().count().saturating_sub(1),
+            })
+            .ok_or_else(|| {
+                anyhow!(
+                    "Common mark / markdown file does not contain a single line"
+                )
+            })?;
+
+        let span = Span { start, end };
+        let source_mapping = indexmap::indexmap! {
+            0..content.chars().count() => span
+        };
+        self.add_inner(
+            origin,
+            vec![CheckableChunk::from_str(content, source_mapping)],
+        );
+        Ok(())
     }
 }
 
@@ -103,15 +143,22 @@ impl From<(ContentOrigin, &str)> for Documentation {
     fn from((origin, content): (ContentOrigin, &str)) -> Self {
         let mut docs = Documentation::new();
 
-        match Clusters::try_from(content) {
-            Ok(cluster) => {
-                let chunks = Vec::<CheckableChunk>::from(cluster);
-                docs.add(origin, chunks);
+        match &origin {
+            ContentOrigin::RustDocTest(path, span) => {
+                if let Ok(excerpt) = crate::util::load_span_from(&mut content.as_bytes(), span.clone()) {
+                    docs.add_rust(origin.clone(), excerpt.as_str())
+                } else {
+                    // TODO
+                    Ok(())
+                }
             }
-            Err(e) => {
-                log::error!("BUG: Failed to create cluster from {}: {}", &origin, e);
-            }
-        }
+            ContentOrigin::RustSourceFile(_path) => docs.add_rust(origin, content),
+            ContentOrigin::CommonMarkFile(_path) => docs.add_commonmark(origin, content),
+            #[cfg(test)]
+            ContentOrigin::TestEntityRust => docs.add_rust(origin, content),
+            #[cfg(test)]
+            ContentOrigin::TestEntityCommonMark => docs.add_commonmark(origin, content),
+        }.unwrap_or_else(|e| warn!("BUG! << failed to load yada >> {}", e));
         docs
     }
 }
@@ -139,8 +186,7 @@ mod tests {
         const TEST_RAW: &str = r#" **A** _very_ good test."#;
         const TEST_PLAIN: &str = r#"A very good test."#;
 
-        let test_path = PathBuf::from("/tmp/dummy");
-        let origin = ContentOrigin::RustSourceFile(test_path.clone());
+        let origin = ContentOrigin::TestEntityRust;
         let docs = Documentation::from((origin.clone(), TEST_SOURCE));
         assert_eq!(docs.index.len(), 1);
         let chunks = docs.index.get(&origin).expect("Must contain dummy path");
@@ -260,83 +306,186 @@ mod tests {
         fn file_justtwo() {
             end2end_rustfile!("demo/src/nested/justtwo.rs", 2);
         }
-    }
 
-    // use crate::literalset::tests::{annotated_literals,gen_literal_set};
-    use crate::checker::dummy::DummyChecker;
-    use crate::documentation::Documentation;
+        // use crate::literalset::tests::{annotated_literals,gen_literal_set};
+        use crate::checker::dummy::DummyChecker;
+        use crate::documentation::Documentation;
 
-    #[cfg(feature = "hunspell")]
-    #[test]
-    fn end2end_chunk() {
-        let _ = env_logger::builder()
-            .is_test(true)
-            .filter(None, log::LevelFilter::Trace)
-            .try_init();
+        #[test]
+        fn end2end_rustfile() {
+            let _ = env_logger::builder()
+                .is_test(true)
+                .filter(None, log::LevelFilter::Trace)
+                .try_init();
 
-        // raw source
-        const SOURCE: &'static str = r#"/// A headline.
+            // raw source
+            const SOURCE: &'static str = r#"/// A headline.
 ///
 /// Erronbeous **bold** __uetchkp__
 struct X"#;
 
-        // extracted content as present as provided by `chunk.as_str()`
-        const RAW: &'static str = r#" A headline.
+            // extracted content as present as provided by `chunk.as_str()`
+            const RAW: &'static str = r#" A headline.
 
  Erronbeous **bold** __uetchkp__"#;
 
-        // markdown erased residue
-        const PLAIN: &'static str = r#"A headline.
+            // markdown erased residue
+            const PLAIN: &'static str = r#"A headline.
 
 Erronbeous bold uetchkp"#;
 
-        let origin = ContentOrigin::RustSourceFile(PathBuf::from("/tmp/virtual"));
-        let docs = Documentation::from((origin.clone(), SOURCE));
+            let origin = ContentOrigin::RustSourceFile(PathBuf::from("/tmp/virtual"));
+            let docs = Documentation::from((origin.clone(), SOURCE));
 
-        // TODO contains utter garbage, should be individual tokens, but is multiple literal
-        let suggestion_set = dbg!(DummyChecker::check(&docs, &())).expect("Must not error");
-        let (origin2, chunks) = docs.iter().next().expect("Must contain exactly one origin");
-        assert_eq!(&origin, origin2);
+            // TODO contains utter garbage, should be individual tokens, but is multiple literal
+            let suggestion_set =
+                dbg!(DummyChecker::check(&docs, &())).expect("Dummy checker never fails. qed");
+            let (origin2, chunks) = docs
+                .iter()
+                .next()
+                .expect("Introduced exactly one source. qed");
+            assert_eq!(&origin, origin2);
 
-        let chunk = chunks
-            .iter()
-            .next()
-            .expect("Must contain exactly one chunk");
+            let chunk = chunks
+                .iter()
+                .next()
+                .expect("Must contain exactly one chunk");
 
-        assert_eq!(chunks.len(), 1);
-        assert_eq!(RAW, chunk.as_str());
+            assert_eq!(chunks.len(), 1);
+            assert_eq!(RAW, chunk.as_str());
 
-        let plain = chunk.erase_markdown();
-        assert_eq!(PLAIN, plain.as_str());
+            let plain = chunk.erase_markdown();
+            assert_eq!(PLAIN, plain.as_str());
 
-        let mut it = suggestion_set.iter();
-        let (_, suggestions) = it.next().expect("Must contain at least one file entry");
+            let mut it = suggestion_set.iter();
+            let (_, suggestions) = it.next().expect("Must contain at least one file entry");
 
-        let mut it = suggestions.into_iter();
-        let mut expected = |word: &'static str| {
-            let suggestion = it.next().expect("Must contain another mis-spelled word");
-            let _s = dbg!(suggestion.chunk.as_str());
+            let mut it = suggestions.into_iter();
+            let mut expected = |word: &'static str| {
+                let suggestion = it.next().expect("Must contain another mis-spelled word");
+                let _s = dbg!(suggestion.chunk.as_str());
 
-            // range for chunk
-            let range: Range = suggestion
-                .span
-                .to_content_range(&suggestion.chunk)
-                .expect("Must work to derive content range from chunk and span");
+                // range for chunk
+                let range: Range = suggestion
+                    .span
+                    .to_content_range(&suggestion.chunk)
+                    .expect("Must work to derive content range from chunk and span");
 
-            log::info!(
-                "Foxxy funkster: {}",
-                suggestion.chunk.display(range.clone())
-            );
+                log::info!(
+                    "Foxxy funkster: {}",
+                    suggestion.chunk.display(range.clone())
+                );
 
-            let _alternative = load_span_from(SOURCE.as_bytes(), suggestion.span.clone())
-                .expect("Span loading must succeed");
+                let _alternative = load_span_from(SOURCE.as_bytes(), suggestion.span.clone())
+                    .expect("Span loading must succeed");
 
-            assert_eq!(word, crate::util::sub_chars(chunk.as_str(), range));
-            log::info!("Found word >> {} <<", word);
-        };
+                assert_eq!(word, crate::util::sub_chars(chunk.as_str(), range));
+                log::info!("Found word >> {} <<", word);
+            };
 
-        expected("A");
-        // expected(" A headline.\n///\n/// Erronbeous ");
-        expected("headline");
+            expected("A");
+            // expected(" A headline.\n///\n/// Erronbeous ");
+            expected("headline");
+        }
+
+        #[test]
+        fn fff() {
+            let _ = env_logger::builder()
+                .is_test(true)
+                .filter(None, log::LevelFilter::Trace)
+                .try_init();
+
+            // raw source
+            const SOURCE: &'static str = r#"# cmark test
+
+A raelly boring test.
+
+## Engineering
+
+```rust
+I am so code!
+```
+
+---
+
+The end.🐢"#;
+
+            // extracted content as present as provided by `chunk.as_str()`
+            const RAW: &'static str = SOURCE;
+
+            // markdown erased residue
+            const PLAIN: &'static str = r#"cmark test
+
+A raelly boring test.
+
+Engineering
+
+
+The end.🐢"#;
+
+            let origin = ContentOrigin::CommonMarkFile(PathBuf::from("/tmp/virtual"));
+            let docs = Documentation::from((origin.clone(), SOURCE));
+
+            let suggestion_set =
+                dbg!(DummyChecker::check(&docs, &())).expect("Dummy checker never fails. qed");
+
+            let (origin2, chunks) = docs
+                .iter()
+                .next()
+                .expect("Introduced exactly one source. qed");
+            assert_eq!(&origin, origin2);
+
+            let chunk = chunks
+                .iter()
+                .next()
+                .expect("Cmark files are always `1+k` chunks, wher `k` is the number of rust(!) code blocks. qed");
+
+            assert_eq!(chunks.len(), 1);
+            assert_eq!(RAW, chunk.as_str());
+
+            let plain = chunk.erase_markdown();
+            assert_eq!(PLAIN, plain.as_str());
+
+            let mut it = suggestion_set.iter();
+            let (_, suggestions) = it.next().expect("Must contain at least one file entry");
+
+            let mut it = suggestions.into_iter();
+
+            let mut expected = |word: &'static str| {
+                let suggestion = it.next().expect("Must contain another mis-spelled word");
+                let _s = dbg!(suggestion.chunk.as_str());
+
+                // range for chunk
+                let range: Range = suggestion
+                    .span
+                    .to_content_range(&suggestion.chunk)
+                    .expect("Must work to derive content range from chunk and span");
+
+                log::info!(
+                    "Foxxy funkster: {}",
+                    suggestion.chunk.display(range.clone())
+                );
+
+                let _alternative = load_span_from(SOURCE.as_bytes(), suggestion.span.clone())
+                    .expect("Span loading must succeed");
+
+                assert_eq!(word, crate::util::sub_chars(chunk.as_str(), range));
+                log::info!("Found word >> {} <<", word);
+            };
+
+            expected("cmark");
+            expected("test");
+
+            expected("A");
+            expected("raelly");
+            expected("boring");
+            expected("test");
+
+            expected("Engineering");
+
+            expected("The");
+            expected("end");
+            expected("🐢");
+        }
     }
 }
