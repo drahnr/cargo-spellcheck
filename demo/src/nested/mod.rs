e969c523f34e529001e44b9b17b26749867d607e
Korbinian Maier <korbinian.maier@posteo.de>
2020-09-14 11:28:16 +0200

    test/demo: add test struct with too long lines

--- demo/src/nested/mod.rs
+++ demo/src/nested/mod.rs
@@ -7,3 +7,8 @@ mod again;
 
 /// Nested;
 struct Nest;
+
+/// Overly long statements that should be reflown since they are __very__ long and exceed the line limit.
+///
+/// This struct has a lot of documentation but unfortunately, the lines are just too long.
+struct SomeLong;
