04cf6292f3a78937b4bfd0ce3216f303fc785744
Korbinian Maier <korbinian.maier@posteo.de>
2020-09-15 11:00:16 +0200

    fix/reflow: check if reflow happend was bad

--- demo/src/nested/mod.rs
+++ demo/src/nested/mod.rs
@@ -12,3 +12,7 @@ struct Nest;
 ///
 /// This struct has a lot of documentation but unfortunately, the lines are just too long.
 struct SomeLong;
+
+/// A long documentation which is short enough for two lines
+/// but too long for one line.
+struct TooLong;
