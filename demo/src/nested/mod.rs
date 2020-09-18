83a91b0460c46a0c37d22603617fd6ca632d7036
Korbinian Maier <korbinian.maier@posteo.de>
2020-09-18 12:25:08 +0200

    test/demo: added a long line with indentaion

--- demo/src/nested/mod.rs
+++ demo/src/nested/mod.rs
@@ -11,7 +11,11 @@ struct Nest;
 /// Overly long statements that should be reflown since they are __very__ long and exceed the line limit.
 ///
 /// This struct has a lot of documentation but unfortunately, the lines are just too long.
-struct SomeLong;
+struct SomeLong {
+    /// This member is interesting though since it has some
+    /// indentation. These whitespaces must be kept.
+    member: i8,
+};
 
 /// A long documentation which is short enough for two lines
 /// but too long for one line.
