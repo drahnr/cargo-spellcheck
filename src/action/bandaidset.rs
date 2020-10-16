133c240386de83c41a4bfba351f07506553280ca
Korbinian Maier <korbinian.maier@posteo.de>
2020-10-16 17:09:56 +0200

    refactor/Bandaid,Reflow: use enum for replacement

--- src/action/bandaidset.rs
+++ src/action/bandaidset.rs
@@ -61,7 +61,7 @@ impl TryFrom<(&Span, Replacement, CommentVariant)> for FirstAidKit {
         (span, replacement, variant): (&Span, Replacement, CommentVariant),
     ) -> Result<Self> {
         if span.is_multiline() {
-            bail!("Construction of `FirstAidKit` from single-line span only")
+            bail!("Can't construct `FirstAidKit` from single-line span only")
         } else {
             let bandaid = BandAid::Replacement(
                 *span,
