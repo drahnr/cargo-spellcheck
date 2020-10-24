c79865a0b22fb3d5fd4b946e0854f444a518006b
Korbinian Maier <korbinian.maier@posteo.de>
2020-10-24 12:20:46 +0200

    test/demo: add anoher long demo coment

--- demo/src/nested/mod.rs
+++ demo/src/nested/mod.rs
@@ -16,6 +16,9 @@ struct SomeLong {
     member: i8,
     #[ doc = "This member is interesting though since it has some indentation. These whitespaces must be kept."]
     sec: i8,
+    #[doc=r###"And a different interesting thing
+    because we have a random newline here?!"###]
+    third: String,
 }
 
 /// A long documentation which is short enough for two lines
@@ -26,3 +29,15 @@ struct TooLong;
 /// instead of three, as it was
 /// initially.
 struct TooShort;
+
+#[ doc = "A long comment which we wanna reflow. So it's Saturday, are you having any plans for tonight?" ]
+struct Someodo;
+
+#[ doc= r#"A long comment which we wanna reflow. So it's Saturday, are you having any plans for 
+           tonight? We're gonna end up with three lines here I think."#]
+struct AnotherSomeodo;
+
+#[ doc= r#"A long short
+comment which we wanna reflow
+to one line."#]
+struct AnotherSomeodo2;
