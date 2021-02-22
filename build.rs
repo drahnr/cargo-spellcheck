2b9574b6280f2e328124a881d41c5ad11c094379
Bernhard Schuster <bernhard@ahoi.io>
2021-02-22 08:22:41 +0100

    chore/nlprule: cargo include was wrong, extra hoops unnecessary

--- build.rs
+++ build.rs
@@ -13,6 +13,9 @@ fn main() -> std::result::Result<(), Box<(dyn std::error::Error + 'static)>> {
 
     #[cfg(feature = "nlprules")]
     {
+        const COMPRESSION_EXTENSION: &str = "xz";
+        const ARTIFACTS_DIR: &str = "nlprule-data";
+
         println!("cargo:rerun-if-changed=nlprule-data/en_rules.bin.xz");
         println!("cargo:rerun-if-changed=nlprule-data/en_tokenizer.bin.xz");
 
@@ -21,38 +24,13 @@ fn main() -> std::result::Result<(), Box<(dyn std::error::Error + 'static)>> {
 
         let cwd = env::current_dir().expect("Current dir must exist. qed");
 
-        const COMPRESSION_EXTENSION: &str = "xz";
-        const VERSION: &str = "0.4.6";
-        const ARTIFACTS_DIR: &str = "nlprule-data";
-
-        let artifacts = cwd.join(ARTIFACTS_DIR);
-        let cache_dir = if cfg!(feature = "artifacts") {
-            // update the artifacts in git
-            Some(artifacts)
-        } else {
-            // since cargo publish is not happy about any files outside
-            // of $OUT being touched with the build.rs
-            let artifacts = artifacts.join(VERSION).join("en");
-            let tmp_dest = out.join(ARTIFACTS_DIR);
-            let tmp_dist_sub = tmp_dest.join(VERSION).join("en");
-            fs::create_dir_all(&tmp_dist_sub)?;
-
-            let cpy = |from: &PathBuf, to: &PathBuf, what: &str| -> io::Result<()> {
-                fs::copy(dbg!(from.join(what)), dbg!(to.join(what)))?;
-                Ok(())
-            };
-            cpy(&artifacts, &tmp_dist_sub, "en_rules.bin.xz")?;
-            cpy(&artifacts, &tmp_dist_sub, "en_tokenizer.bin.xz")?;
-            Some(tmp_dest)
-        };
+        let cache_dir = Some(cwd.join(ARTIFACTS_DIR));
 
         nlprule_build::BinaryBuilder::new(&["en"], &out)
-            .version(VERSION)
             .fallback_to_build_dir(false)
             .cache_dir(cache_dir)
             .transform(
                 &|source, mut sink| {
-                    eprintln!("Calling transform data");
                     let mut encoder = XzEncoder::new(BufReader::new(source), 9);
                     std::io::copy(&mut encoder, &mut sink)?;
                     Ok(())
@@ -67,7 +45,6 @@ fn main() -> std::result::Result<(), Box<(dyn std::error::Error + 'static)>> {
                     ext.push_str(".");
                     ext.push_str(COMPRESSION_EXTENSION);
                     path.set_extension(ext);
-                    eprintln!("transform: {}", path.display());
                     Ok(path)
                 })
             .build()?
@@ -78,15 +55,13 @@ fn main() -> std::result::Result<(), Box<(dyn std::error::Error + 'static)>> {
                     Ok(())
                 },
                 |mut path: PathBuf| -> PathBuf {
-                    eprintln!("Calling postprocess data");
                     let ext = path.extension()
                         .map(|s| { s.to_os_string().into_string().expect("Extension conversion from OSString to regular string works. qed") })
                         .unwrap_or_else(|| String::with_capacity(4));
 
-                    assert!(dbg!(&ext).ends_with(COMPRESSION_EXTENSION));
+                    assert!(&ext.ends_with(COMPRESSION_EXTENSION));
                     let k = ext.len().saturating_sub(COMPRESSION_EXTENSION.len() + 1);
                     path.set_extension(&ext[..k]);
-                    eprintln!("postprocess: {}", path.display());
                     path
                 }
             )?
