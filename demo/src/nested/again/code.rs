72fad2e649d36f36a71596562defca5e73c4d6f7
Bernhard Schuster <bernhard@ahoi.io>
2021-04-09 11:55:37 +0200

    chores: minor cleanups

---
+++ demo/src/nested/again/code.rs
@@ -0,0 +1,8 @@
+//!
+//! Enclozed `codez` not checked.
+//!
+
+
+/// Do you like `wroeng` very mouch?
+/// Sharon stayed home from school the other day. Because she was sick.
+struct Coda;
--- demo/src/nested/again/mod.rs
+++ demo/src/nested/again/mod.rs
@@ -1,2 +1,4 @@
+mod code;
+
 /// Again.
 struct Again;
