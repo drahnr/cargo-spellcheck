970b888a536cc2b537f6e6818a5f47ac30721f5a
Bernhard Schuster <bernhard@ahoi.io>
2021-07-09 11:41:15 +0200

    workaround: assure the tokenizer recognizes i.e. isn't as one token

--- src/checker/tokenize.rs
+++ src/checker/tokenize.rs
@@ -76,18 +76,61 @@ pub(crate) fn apply_tokenizer<'t, 'z>(
 where
     't: 'z,
 {
+
     tokenizer
         .pipe(text)
         .into_iter()
         .map(|sentence| {
-            sentence.into_iter().filter_map(|token| {
-                let char_range = token.span().char();
-                if char_range.is_empty() {
-                    None
-                } else {
-                    Some(char_range.clone())
+            let mut backlog = Vec::with_capacity(4);
+            let mut acc = Vec::with_capacity(32);
+            let mut iter = sentence
+                .into_iter()
+                .filter(|token| !token.span().char().is_empty())
+                .peekable();
+
+            // special cases all abbreviated variants, i.e. `isn't` such
+            // that the tokenizer treats them as a single word.
+            while let Some(token) = iter.next() {
+                let char_range = token.span().char().clone();
+                backlog.push(char_range);
+                if let Some(upcoming) = iter.peek() {
+                    let s = upcoming.word().text().as_str();
+                    if s == "'" && !upcoming.has_space_before() {
+                        backlog.push(upcoming.span().char().clone());
+                        let _ = iter.next();
+                        if let Some(upcoming2) = iter.peek() {
+                            let s = upcoming2.word().text().as_str();
+                            if s.len() == 1 && !upcoming2.has_space_before() {
+                                acc.push(backlog.first().unwrap().start .. upcoming2.span().char().end);
+                                backlog.clear();
+                                let _ = iter.next();
+                                continue;
+                            }
+                        }
+                    }
                 }
-            })
+                acc.extend(backlog.drain(..));
+            }
+            acc.into_iter()
         })
         .flatten()
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tokenizer_for_abbrev() {
+        let tok = tokenizer::<PathBuf>(None).unwrap();
+        let ranges = apply_tokenizer(&tok, "It isn't that different.");
+
+        ranges
+            .zip([0_usize..2, 3..8, 9..13, 14..23, 23..24].iter().cloned())
+            .for_each(|(is, expect)| {
+                assert_eq!(is, expect);
+            });
+    }
 }
