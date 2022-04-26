5bb6279416c8a4ac439217789e75c956c4c99f5b
Bernhard Schuster <bernhard@ahoi.io>
2022-04-26 08:16:56 +0200

    chore: parse Args and pass to fn run

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use log::warn;
 
-use cargo_spellcheck::{action, errors::Result, run};
+use cargo_spellcheck::{action, errors::Result, run, Args};
 
 #[allow(missing_docs)]
 fn main() -> Result<()> {
     let _ = color_eyre::install()?;
-    let res = run();
+    let args = Args::parse(std::env::args()).unwrap_or_else(|e| e.exit());
+    let res = run(args);
     // no matter what, restore the terminal
     if let Err(e) = action::interactive::ScopedRaw::restore_terminal() {
         warn!("Failed to restore terminal: {}", e);
